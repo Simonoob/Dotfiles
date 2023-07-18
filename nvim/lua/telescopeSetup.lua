4d021c64f1eef2abf7b887b0a596843509c4611a
simonoob <simonefiorentini1@gmail.com>
2023-07-18 10:59:41 +0300

    feat: change telescope keymaps

--- nvim/lua/telescopeSetup.lua
+++ nvim/lua/telescopeSetup.lua
@@ -14,20 +14,31 @@ require("telescope").setup({
 -- Enable telescope fzf native, if installed
 pcall(require("telescope").load_extension, "fzf")
 
+require("which-key").register({
+	f = {
+		name = "Find", -- optional group name
+	},
+}, { prefix = "<leader>" })
 -- See `:help telescope.builtin`
-vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
-vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
-vim.keymap.set("n", "<leader>/", function()
+vim.keymap.set("n", "<leader>fh", require("telescope.builtin").oldfiles, { desc = "[H]istory" })
+vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, { desc = "[B]uffers" })
+vim.keymap.set("n", "<leader>f/", function()
 	-- You can pass additional configuration to telescope to change theme, layout, etc.
 	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 		winblend = 10,
 		previewer = false,
 	}))
-end, { desc = "[/] Fuzzily search in current buffer" })
+end, { desc = "[F]uzzily [/]search in current buffer" })
 
-vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
-vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
-vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+
+require("which-key").register({
+	g = {
+		name = "Git", -- optional group name
+	},
+}, { prefix = "<leader>" })
+vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "[F]iles" })
+vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "[F]iles" })
+vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = " [H]elp" })
+vim.keymap.set("n", "<leader>fw", require("telescope.builtin").grep_string, { desc = "[W]ord" })
+vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "[G]rep" })
+vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, { desc = "[D]iagnostics" })
