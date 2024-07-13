134f687906524fe9765cbe6ec487a5fef631b2c1
simonoob <simonefiorentini1@gmail.com>
2024-07-13 11:16:39 +0300

    feat(markdown): add more complex keymaps with fn :tada:

--- common/nvim/after/ftplugin/markdown.lua
+++ common/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,44 @@
+-- UTILS --------------------------
+local function write_at_cursor(text)
+  local column_pos = vim.api.nvim_win_get_cursor(0)[2]
+  local current_line = vim.api.nvim_get_current_line()
+  local nline = current_line:sub(0, column_pos) .. text .. current_line:sub(column_pos + 1)
+  vim.api.nvim_set_current_line(nline)
+end
+
+local function write_above_cursor(text, new_line)
+  new_line = new_line or true
+  local column_pos = vim.api.nvim_win_get_cursor(0)[2]
+  local line_num = vim.api.nvim_win_get_cursor(0)[1]
+
+  if new_line then
+    local nline = string.rep(' ', column_pos) .. text
+    vim.api.nvim_buf_set_lines(0, line_num - 1, line_num - 1, false, { nline })
+  else
+    local prev_line = vim.api.nvim_buf_get_lines(0, line_num - 2, line_num - 1, false)[1]
+    local nline = prev_line:sub(0, column_pos) .. text .. prev_line:sub(column_pos + 1)
+    vim.api.nvim_set_current_line(nline)
+  end
+  vim.api.nvim_win_set_cursor(0, { line_num, column_pos })
+end
+-- END UTILS --------------------------
+
+local function write_below_cursor(text, new_line)
+  new_line = new_line or true
+  local column_pos = vim.api.nvim_win_get_cursor(0)[2]
+  local line_num = vim.api.nvim_win_get_cursor(0)[1]
+
+  if new_line then
+    local nline = string.rep(' ', column_pos) .. text
+    vim.api.nvim_buf_set_lines(0, line_num + 1, line_num + 1, false, { nline })
+  else
+    local next_line = vim.api.nvim_buf_get_lines(0, line_num + 1, line_num + 2, false)[1]
+    local nline = next_line:sub(0, column_pos) .. text .. next_line:sub(column_pos + 1)
+    vim.api.nvim_set_current_line(nline)
+  end
+  vim.api.nvim_win_set_cursor(0, { line_num + 1, column_pos })
+end
+
 require('which-key').register {
   ['<leader>m'] = { name = '[M]arkdown', _ = 'which_key_ignore' },
 }
@@ -14,11 +55,26 @@ vim.keymap.set('n', '<leader>mi', '2i*<Esc>i', { desc = '[I]talic' })
 vim.keymap.set('n', '<leader>mb', '4i*<Esc>2<Left>a', { desc = '[B]old' })
 
 vim.keymap.set('n', '<leader>mc', 'o- [ ] ', { desc = '[C]heckbox' })
-vim.keymap.set('n', '<leader>mt', 'o| # | # | # |<Esc>o|----|----|----|<Esc>o| # | # | # |<Esc>2k0/#<CR>', { desc = '[T]able' })
+
+vim.keymap.set('n', '<leader>mt', function()
+  local line_num = vim.api.nvim_win_get_cursor(0)[1]
+  vim.api.nvim_win_set_cursor(0, { line_num, 0 })
+  write_below_cursor '| fill_heading | fill_heading | fill_heading |'
+  write_below_cursor '|--------|--------|--------|'
+  write_below_cursor '| fill_cell | fill_cell | fill_cell |'
+  vim.cmd [[/fill\w*]]
+end, { desc = '[T]able' })
 
 require('which-key').register {
   ['<leader>ma'] = { name = '[A]rrow', _ = 'which_key_ignore' },
 }
-vim.keymap.set('n', '<leader>mal', 'a  -->  ', { desc = '[L] Right' })
-vim.keymap.set('n', '<leader>mah', 'i  <--  <Esc>b2hi', { desc = '[H] Left' })
--- vim.keymap.set('n', '<leader>mak', 'i -marker- <Esc>yyP/-marker-<CR>ciW|-marker-<Esc>yyP/-marker-<CR>ciW^<Esc>:%s/-marker-//g<CR>2k', { desc = '[k] Up' })
+vim.keymap.set('n', '<leader>mal', 'a  -->  <Esc>', { desc = '[L] Right' })
+vim.keymap.set('n', '<leader>mah', 'i  <--  <Esc>b2h', { desc = '[H] Left' })
+vim.keymap.set('n', '<leader>mak', function()
+  write_above_cursor '|'
+  write_above_cursor '^'
+end, { desc = '[K] Up' })
+vim.keymap.set('n', '<leader>maj', function()
+  write_below_cursor '|'
+  write_below_cursor 'v'
+end, { desc = '[J] Down' })
