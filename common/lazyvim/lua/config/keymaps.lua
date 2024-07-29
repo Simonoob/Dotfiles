670896ee334a465fe2877ebd179c16339eaddf54
simonoob <simonefiorentini1@gmail.com>
2024-07-29 12:58:04 +0300

    feat(keymaps): add delete all marks keymap

--- common/lazyvim/lua/config/keymaps.lua
+++ common/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+--Add any additional keymaps here
+
+require("which-key").add({ "<leader>d", "which_key_ignore", desc = "delete", mode = "n" })
+vim.keymap.set("n", "<leader>dm", "<Esc>:delm! | delm A-Z0-9<CR>", { desc = "delete all marks" })
