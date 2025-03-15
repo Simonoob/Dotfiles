75da60ba69aafbdde6f9cb02381c4684c82417be
simonoob <simonefiorentini1@gmail.com>
2025-03-15 12:38:29 +0200

    disable git signs

--- common/nvim-new/lua/plugins/git.lua
+++ common/nvim-new/lua/plugins/git.lua
@@ -3,13 +3,13 @@ local gitsigns = {
 	-- Adds git related signs to the gutter, as well as utilities for managing changes
 	"lewis6991/gitsigns.nvim",
 	opts = {
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-		},
+		-- signs = {
+		-- 	add = { text = "+" },
+		-- 	change = { text = "~" },
+		-- 	delete = { text = "_" },
+		-- 	topdelete = { text = "‾" },
+		-- 	changedelete = { text = "~" },
+		-- },
 
 		on_attach = function(buffer)
 			local gs = package.loaded.gitsigns
