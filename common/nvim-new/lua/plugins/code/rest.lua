20c65da57d9b142069361b2576e88d69aa68c6b5
simonoob <simonefiorentini1@gmail.com>
2025-03-16 11:23:47 +0200

    feat(nvim): autopair html tags

--- common/nvim-new/lua/plugins/code/rest.lua
+++ common/nvim-new/lua/plugins/code/rest.lua
@@ -12,6 +12,27 @@ local auto_pairs = {
 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 	end,
 }
+local auto_pair_html_tags = {
+	"windwp/nvim-ts-autotag",
+	config = function()
+		require("nvim-ts-autotag").setup({
+			opts = {
+				-- Defaults
+				enable_close = true, -- Auto close tags
+				enable_rename = true, -- Auto rename pairs of tags
+				enable_close_on_slash = false, -- Auto close on trailing </
+			},
+			-- Also override individual filetype configs, these take priority.
+			-- Empty by default, useful if one of the "opts" global settings
+			-- doesn't work well in a specific filetype
+			-- per_filetype = {
+			-- 	["html"] = {
+			-- 		enable_close = false,
+			-- 	},
+			-- },
+		})
+	end,
+}
 
 local auto_detect_tabs = {
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
@@ -38,5 +59,6 @@ local inc_rename = {
 return {
 	auto_detect_tabs,
 	auto_pairs,
+	auto_pair_html_tags,
 	inc_rename,
 }
