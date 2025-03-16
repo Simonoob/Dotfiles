eab2518e05c5ab4ff2b70b9ee925cf9a880c897f
simonoob <simonefiorentini1@gmail.com>
2025-03-16 10:52:58 +0200

    feat(nvim): adjust autopairs

--- common/nvim-new/lua/plugins/code/rest.lua
+++ common/nvim-new/lua/plugins/code/rest.lua
@@ -2,8 +2,11 @@ local auto_pairs = {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
 	config = function()
-		require("nvim-autopairs").setup({})
-		-- If you want to automatically add `(` after selecting a function or method
+		require("nvim-autopairs").setup({
+			disable_filetype = { "TelescopePrompt", "vim" },
+			ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
+		})
+		-- automatically add `(` after selecting a function or method
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		local cmp = require("cmp")
 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
