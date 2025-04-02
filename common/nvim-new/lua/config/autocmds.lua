8fe4ab20389e96db13cdb31b565ab6d53e1fa5a9
simonoob <simonefiorentini1@gmail.com>
2025-04-02 20:35:43 +0300

    chore:

--- common/nvim-new/lua/config/autocmds.lua
+++ common/nvim-new/lua/config/autocmds.lua
@@ -13,24 +13,25 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- < map CapsLock to Esc key when within neovim >
--- IMPORTANT: this is macOS specific
-local function remapCapsLockToEscape()
-	os.execute(
-		'hidutil property --set \'{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029}]}\''
-	)
-end
-
-local function unmapCapsLockToEscape()
-	os.execute(
-		'hidutil property --set \'{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000029,"HIDKeyboardModifierMappingDst":0x700000039}]}\''
-	)
-end
-
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = remapCapsLockToEscape,
-})
-
-vim.api.nvim_create_autocmd("VimLeave", {
-	callback = unmapCapsLockToEscape,
-})
+-- IMPORTANT: this is macOS specific - actually on my macOS I used the builtin `customise modifier keys` from the options, otherwise I would get conflics when switching to the terminal from within neovim
+--
+-- local function remapCapsLockToEscape()
+-- 	os.execute(
+-- 		'hidutil property --set \'{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029}]}\''
+-- 	)
+-- end
+--
+-- local function unmapCapsLockToEscape()
+-- 	os.execute(
+-- 		'hidutil property --set \'{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000029,"HIDKeyboardModifierMappingDst":0x700000039}]}\''
+-- 	)
+-- end
+--
+-- vim.api.nvim_create_autocmd("VimEnter", {
+-- 	callback = remapCapsLockToEscape,
+-- })
+--
+-- vim.api.nvim_create_autocmd("VimLeave", {
+-- 	callback = unmapCapsLockToEscape,
+-- })
 -- </ map CapsLock to Esc key when within neovim >
