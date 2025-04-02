aef07db0dec1d664eacf6b5ed7608a0e0d8c97a2
simonoob <simonefiorentini1@gmail.com>
2025-04-02 20:31:57 +0300

    feat(keymaps): remap caps-lock to esc key

--- common/nvim-new/lua/config/autocmds.lua
+++ common/nvim-new/lua/config/autocmds.lua
@@ -11,3 +11,26 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- < map CapsLock to Esc key when within neovim >
+-- IMPORTANT: this is macOS specific
+local function remapCapsLockToEscape()
+	os.execute(
+		'hidutil property --set \'{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029}]}\''
+	)
+end
+
+local function unmapCapsLockToEscape()
+	os.execute(
+		'hidutil property --set \'{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000029,"HIDKeyboardModifierMappingDst":0x700000039}]}\''
+	)
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = remapCapsLockToEscape,
+})
+
+vim.api.nvim_create_autocmd("VimLeave", {
+	callback = unmapCapsLockToEscape,
+})
+-- </ map CapsLock to Esc key when within neovim >
