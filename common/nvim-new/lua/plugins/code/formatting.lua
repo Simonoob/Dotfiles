a726b85e293ef358dc104bc43b8511aa0c91020f
simonoob <simonefiorentini1@gmail.com>
2025-05-12 13:30:40 +0300

    fix(conform): autoformatting in python

--- common/nvim-new/lua/plugins/code/formatting.lua
+++ common/nvim-new/lua/plugins/code/formatting.lua
@@ -66,7 +66,7 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { "stylua" },
       -- Conform can also run multiple formatters sequentially
-      python = { "ruff" },
+      python = { "ruff_fix", "ruff_format" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       javascript = { "eslint_d", "prettierd" },
